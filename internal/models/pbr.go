2c88ee9c35e0cbe44c748da71868aa15a41a4a4b
akos011221 <orbanakos2001@gmail.com>
2025-04-04 18:08:35 +0200

    feat(pbr): Placeholder functions for the PBR model
    Create placeholder functions for the PBR model—, GetPolicyBasedRoutes that will interact with the GCP API through the Go GCP client library to get the active PBRs from the input project's input VPC and CreatePolicyBasedRoute which will be used to create the missing PBRs.

--- internal/models/pbr.go
+++ internal/models/pbr.go
@@ -8,3 +8,19 @@ type PolicyBasedRoute struct {
 	NextHopType string `json:"next_hop_type"`
 	NextHop     string `json:"next_hop"`
 }
+
+// GetPolicyBasedRoutes takes a project ID and a VPC id and returns a slice of PolicyBasedRoute
+// objects from GCP data.
+func GetPolicyBasedRoutes(projectID string, vpc string) ([]PolicyBasedRoute, error) {
+	// Later, I'll add the logic here to get the PBRs from the project
+	// using the GCP Go client library.
+	return nil, nil
+}
+
+// CreatePolicyBasedRoute takes a project ID, a VPC id and a PolicyBasedRoute to create a new
+// policy-based route in GCP.
+func CreatePolicyBasedRoute(projectID string, vpc string, pbr PolicyBasedRoute) error {
+	// Later, I'll add the logic here to create the PBR in GCP
+	// using the GCP Go client library.
+	return nil
+}
